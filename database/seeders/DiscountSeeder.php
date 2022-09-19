43ab0a769123664a7a1948f3c0415f4c58d966e8
Tom Geitz <tgeitz@gmail.com>
2022-09-19 18:56:20 -0500

    Add Discount model, migration, and seeder

---
+++ database/seeders/DiscountSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Discount;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiscountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $discounts = [
+            ['discountable_type' => 'product', 'discountable_id' => Product::where('sku', '000003')->firstOrFail()->id, 'discount_percentage' => 15, 'is_active' => true],
+            ['discountable_type' => 'category', 'discountable_id' => Category::where('name', 'insurance')->firstOrFail()->id, 'discount_percentage' => 30, 'is_active' => true],
+        ];
+
+        foreach ($discounts as $discount) {
+            Discount::create([
+                'discountable_type' => $discount['discountable_type'],
+                'discountable_id' => $discount['discountable_id'],
+                'discount_percentage' => $discount['discount_percentage'],
+                'is_active' => $discount['is_active'],
+            ]);
+        }
+    }
+}
