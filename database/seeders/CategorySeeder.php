3cdb9f5456d6fdfdfce02dddc9f477453c78e4e9
Tom Geitz <tgeitz@gmail.com>
2022-09-19 18:06:14 -0500

    Create Categories model, migration, and seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categoryNames = ['insurance', 'vehicle'];
+        foreach($categoryNames as $name) {
+            Category::create(['name' => $name]);
+        }
+    }
+}
