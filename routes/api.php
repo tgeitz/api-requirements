da454c043c8511944974e26786125d5a57eb3e90
Tom Geitz <tgeitz@gmail.com>
2022-09-20 11:08:59 -0500

    Add /products route to api and create endpoint in ProductsController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//
+Route::get('/products', [ProductsController::class, 'index']);
